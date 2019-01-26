9039a7f71e6ca347de534b1415ff9bbdcd49b41f
motians <motians@comcast.net>
2019-01-26 10:37:57 -0800

    Starting function definitions

---
+++ students/Shawn_DeGraw/lessons/lesson03/assignment/basic_operations.py
@@ -0,0 +1,18 @@
+def add_customer(customer_id, name, lastname, home_address, phone_number, email_address, status, credit_limit):
+    pass
+
+
+def search_customer(customer_id):
+    pass
+
+
+def delete_customer(customer_id):
+    pass
+
+
+def update_customer_credit(customer_id, credit_limit):
+    pass
+
+
+def list_active_customers():
+    pass
