f1d26e58e270643e154362801aefbf44fff5071e
motians <motians@comcast.net>
2019-01-28 21:27:06 -0800

    added error checking to create

--- students/Shawn_DeGraw/lessons/lesson03/assignment/hpnorton_database/createdb.py
+++ students/Shawn_DeGraw/lessons/lesson03/assignment/hpnorton_database/createdb.py
@@ -7,8 +7,12 @@ from lesson03.assignment.hpnorton_database.hpnortondbmodel import *
 
 if __name__ == "__main__":
 
-    DATABASE.create_tables([
-        Customer
-    ])
+    try:
+        DATABASE.connect()
+        DATABASE.create_tables([
+            Customer
+        ])
+    except InternalError:
+        print("Database Error.")
 
     DATABASE.close()
