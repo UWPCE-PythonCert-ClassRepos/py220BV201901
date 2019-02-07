dd50736abc8bfac1fda7d944f133f9856ea94677
Ying Guo <guoguoying2013@gmail.com>
2019-02-06 17:21:49 -0800

    add creat model module

---
+++ students/YingGuo/lessons/lesson03/assignment/creat_model.py
@@ -0,0 +1,14 @@
+from management_database_model import *
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger.info('One off program to build the classes from the model in the database')
+
+database.create_tables([
+        Customer,
+        Sale,
+    ])
+
+database.close()
