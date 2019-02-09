4aba4ab0692525305ecfc3e7f9d93ad976294b31
motians <motians@comcast.net>
2019-02-08 21:25:37 -0800

    Starting test file

---
+++ students/Shawn_DeGraw/lessons/lesson05/assignment/test_database.py
@@ -0,0 +1,12 @@
+"""
+    Lesson05 tests
+    HP Norton products in MongoDB
+
+"""
+
+import pytest
+import lesson05.assignment.hpnorton_products.mongodb_operations as mongo
+
+
+def test_import_date():
+    """ 
