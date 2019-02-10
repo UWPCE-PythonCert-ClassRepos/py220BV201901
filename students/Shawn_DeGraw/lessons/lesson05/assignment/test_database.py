dcbb74b8f966949b8439deb7459e75dc3d3c7003
motians <motians@comcast.net>
2019-02-09 17:17:39 -0800

    test for available products

--- students/Shawn_DeGraw/lessons/lesson05/assignment/test_database.py
+++ students/Shawn_DeGraw/lessons/lesson05/assignment/test_database.py
@@ -4,9 +4,61 @@
 
 """
 
-import pytest
-import lesson05.assignment.hpnorton_products.mongodb_operations as mongo
+import lesson05.assignment.hpnorton_products.database as mongo
 
 
 def test_import_date():
-    """ 
+    """ import data """
+
+    result = mongo.import_data('c:/Users/motia/OneDrive/Documents/PythonCertClass/py220BV201901/students/Shawn_DeGraw/lessons/lesson05/assignment', 'products.csv', 'customers.csv', 'rentals.csv')
+
+    assert result == [(10, 10, 9), (0, 0, 0)]
+
+
+def test_show_avail_products():
+    """ test for available products """
+
+    result = mongo.show_available_products()
+    assert result == {
+                        'prd001':
+                        {
+                            'description':'60-inch TV stand',
+                            'product_type':'livingroom',
+                            'quantity_available':'3'
+                        },
+                        'prd003':
+                        {
+                            'description':'Acacia kitchen table',
+                            'product_type':'kitchen',
+                            'quantity_available':'7'
+                        },
+                        'prd004':
+                        {
+                            'description':'Queen bed',
+                            'product_type':'bedroom',
+                            'quantity_available':'10'
+                        },
+                        'prd005':
+                        {
+                            'description':'Reading lamp',
+                            'product_type':'bedroom',
+                            'quantity_available':'20'
+                        },
+                        'prd006':
+                        {
+                            'description':'Portable heater',
+                            'product_type':'bathroom',
+                            'quantity_available':'14'
+                        },
+                        'prd008':
+                        {
+                            'description':'Smart microwave',
+                            'product_type':'kitchen',
+                            'quantity_available':'30'
+                        },
+                        'prd010':
+                        {
+                            'description':'60-inch TV',
+                            'product_type':'livingroom',
+                            'quantity_available':'3'
+                        }}
