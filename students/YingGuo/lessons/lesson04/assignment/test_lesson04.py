297012329cdf0a4a9f8a5b62aa7d5dc5a04b480e
Ying Guo <guoguoying2013@gmail.com>
2019-02-18 16:15:17 -0800

    updated test_lesson04.py

--- students/YingGuo/lessons/lesson04/assignment/test_lesson04.py
+++ students/YingGuo/lessons/lesson04/assignment/test_lesson04.py
@@ -12,9 +12,21 @@ def test_add_many_customers():
         ["456", "Name", "Lastname", "Address", "phone", "email", "inactive", 10]
     ]
     add_many_customers(list_customers)
-    assert search_customer("123") == {"123": ["Name", "Lastname", "Address", "phone", "email", "active", 999]}
+    print(search_customer("123"))
+    print(search_customer("456"))
+    assert search_customer("456") == {"123": ["Name", "Lastname", "Address", "phone", "email", False, 10]}
 
 def test_search_customer():
     a = search_customer("C000020")
-    result = {"C000020": ["Taylor", "Bradtke", "451", "Gerhold Burgs", "597.487.6813", "Brando.Barton@wilford.com", True, 474]}
+    result = {"C000020": ["Wendy", "Schneider", "58859 Schmeler Wall", "(346)696-3257", "Randi@bridie.info", True, 249]}
     assert a == result
+
+def test_delete_customer():
+    delete_customer("123")
+    assert search_customer("123") == {}
+
+def test_update_customer_credit():
+    update_customer_credit("C000021", 800)
+    a = search_customer("C000021")
+    new_credit = a["C000021"][-1]
+    assert new_credit == 800
