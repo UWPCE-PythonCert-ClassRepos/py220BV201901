e05467ab166ae1301ac0eb0a85de729f187a4424
Tim Meese <tim.meese67@gmail.com>
2019-02-20 20:30:54 -0800

    Catchup: assign04 & assign05 changes

---
+++ students/timm67/lessons/lesson04/assignment/import_csv.py
@@ -0,0 +1,21 @@
+from basic_operations import add_customer
+from basic_operations import search_customer
+from basic_operations import delete_customer
+from basic_operations import update_customer_credit
+from basic_operations import list_active_customers
+
+from loguru import logger
+from sys import stdout
+from csv import reader as csv_reader
+
+# indeces into array returned by CSV reader
+cust_id = 0
+cust_name = 1
+
+def import_csv(csv_filename):
+    with open(csv_filename, 'r') as csv_fd:
+        myreader = csv_reader(csv_fd.readline())
+        line = myreader()
+        while line != []:
+            line = myreader()
+            yield line
---
+++ students/timm67/lessons/lesson04/assignment/main.py
@@ -0,0 +1,39 @@
+from basic_operations import add_customer
+from basic_operations import search_customer
+from basic_operations import delete_customer
+from basic_operations import update_customer_credit
+from basic_operations import list_active_customers
+
+from loguru import logger
+from sys import stdout
+
+
+def main():
+    """
+    Ensure you application will create an empty database if one doesn’t exist
+    when the app is first run. Call it customers.db
+    """
+
+    # Standalone function to initialize logging
+
+    logger.add(stdout, level='INFO')
+    logger.add("logfile_{time}.txt", level='DEBUG')
+    logger.enable(__name__)
+
+    # TODO: load the CSV file using generator and doing profiling
+    import_csv_gen = import_csv('customer_lesson3.csv')
+
+    #
+    # ingest_csv contains the generator function that iterates through the CSV
+    # file parsing the CSV into a list of items
+    #
+    while True:
+        try:
+            db_row = next(ingest_csv_gen)
+            # TODO: add db_row to the database
+        catch StopIteration:
+            break
+
+
+if  __name__ == '__main__':
+    main()
