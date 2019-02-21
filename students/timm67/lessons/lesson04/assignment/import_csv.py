fdb91b7c53673b554bef72ed2930a84bb924e699
Tim Meese <tim.meese67@gmail.com>
2019-02-20 20:47:38 -0800

    Compressed CSV file and import functions for assign04

--- students/timm67/lessons/lesson04/assignment/import_csv.py
+++ students/timm67/lessons/lesson04/assignment/import_csv.py
@@ -8,9 +8,21 @@ from loguru import logger
 from sys import stdout
 from csv import reader as csv_reader
 
+from zipfile import ZipFile
+
 # indeces into array returned by CSV reader
 cust_id = 0
 cust_name = 1
+zip_filename = 'customer.zip'
+csv_filename = 'customer.csv'
+
+def extract_csv(zip_filename):
+    # opening the zip file in READ mode 
+    with ZipFile(zip_filename, 'r') as zip:
+        # extracting all the files 
+        print('Extracting all the files now...') 
+        zip.extractall() 
+
 
 def import_csv(csv_filename):
     with open(csv_filename, 'r') as csv_fd:
