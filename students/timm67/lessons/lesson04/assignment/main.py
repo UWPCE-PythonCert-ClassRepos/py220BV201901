71d24eac40b95f9c1363072ea25bfb309bf2dbd1
Tim Meese <tim.meese67@gmail.com>
2019-02-21 20:54:26 -0800

    main.py changes

--- students/timm67/lessons/lesson04/assignment/main.py
+++ students/timm67/lessons/lesson04/assignment/main.py
@@ -22,19 +22,7 @@ def main():
     logger.enable(__name__)
 
     # TODO: load the CSV file using generator and doing profiling
-    ingest_csv_gen = ingest_csv()
-
-    #
-    # ingest_csv contains the generator function that iterates through the CSV
-    # file parsing the CSV into a list of items
-    #
-    while True:
-        try:
-            db_row = next(ingest_csv_gen)
-            # TODO: add db_row to the database
-        catch StopIteration:
-            break
-
+    ingest_csv()
 
 if  __name__ == '__main__':
     main()
