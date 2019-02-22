6d93b8fa54dbacc247c1f3019d92a1f65c3343fe
yushusongmsazure <yushusong@outlook.com>
2019-02-21 22:47:43 -0800

    finish up mongodb

--- students/yushusongmsazure/lessons/lesson05/assignment/database.py
+++ students/yushusongmsazure/lessons/lesson05/assignment/database.py
@@ -1,3 +1,10 @@
+'''
+Yushu Song
+Assignment 05
+'''
+
+import csv
+import os
 from pymongo import MongoClient
 
 class MongoDBConnection(object):
@@ -24,3 +31,132 @@ class MongoDBConnection(object):
         Close the connection
         """
         self.connection.close()
+
+def import_data(directory_name, product_file, customer_file, rentals_file):
+    '''
+    Read in product, customer, and rental files and populate mongo db collections
+    '''
+    mongo = MongoDBConnection()
+    with mongo:
+        # mongodb database; it all starts here
+        db = mongo.connection.inventory
+        # initialize collection and error count
+        product_added, customer_added, rental_added = 0, 0, 0
+        product_error, customer_error, rental_error = 0, 0, 0
+
+        try:
+            # process products data
+            with open(os.path.join(directory_name, product_file),
+                      encoding='utf-8-sig',
+                      newline='') as prod_file:
+
+                # add customer collection
+                prod_data = csv.DictReader(prod_file)
+                prod_col = db["products"]
+
+                for row in prod_data:
+                    quantity = int(row["quantity_available"])
+                    prod_col.insert_one({"product_id":row["product_id"],
+                                         "description":row["description"],
+                                         "product_type":row["product_type"],
+                                         "quantity_available":quantity})
+                    product_added += 1
+
+        except FileNotFoundError:
+            product_error += 1
+
+        try:
+            # process customers data
+            with open(os.path.join(directory_name, customer_file),
+                      encoding='utf-8-sig',
+                      newline='') as csvfile:
+
+                # add customer collection
+                customer_data = csv.DictReader(csvfile)
+                customer_col = db["customers"]
+                for row in customer_data:
+                    customer_col.insert_one(row)
+                    customer_added += 1
+
+        except FileNotFoundError:
+            customer_error += 1
+
+        try:
+            # process rentals data
+            with open(os.path.join(directory_name, rentals_file),
+                      encoding='utf-8-sig',
+                      newline='') as rent_file:
+
+                # add customer collection
+                rent_data = csv.DictReader(rent_file)
+                rent_col = db["rentals"]
+                for rent in rent_data:
+                    rent_col.insert_one(rent)
+                    rental_added += 1
+
+        except FileNotFoundError:
+            rental_error += 1
+
+    return ((product_added, customer_added, rental_added),
+            (product_error, customer_error, rental_error))
+
+def show_available_products():
+    '''
+    Show all the products available to rent
+    '''
+    mongo = MongoDBConnection()
+    with mongo:
+        mongo_db = mongo.connection.inventory
+        prod_col = mongo_db["products"]
+        products = {}
+
+        for product in prod_col.find({"quantity_available": {"$gt": 0}}):
+            products[product["product_id"]] = {"description":product["description"],
+                                               "product_type":product["product_type"],
+                                               "quantity_available":product["quantity_available"]}
+        return products
+
+def show_rentals(product_id):
+    '''
+    Given a product id find all the users who made a rental
+    '''
+    mongo = MongoDBConnection()
+    with mongo:
+        mongo_db = mongo.connection.inventory
+        customer_col = mongo_db["customers"]
+        rental_col = mongo_db["rentals"]
+        users = {}
+
+        for rent in rental_col.find({"product_id": product_id}):
+            user_id = rent["user_id"]
+            customer = customer_col.find_one({"user_id": user_id})
+            users[user_id] = {"name":customer["name"],
+                              "address":customer["address"],
+                              "phone_number":customer["phone_number"],
+                              "email":customer["email"]}
+    return users
+
+def print_dict(data):
+    '''
+    Print items in a dictionary
+    '''
+    for key, value in data.items():
+        print(f"{key}: {value}")
+
+def main():
+    '''
+    Define main flow
+    '''
+    added, errors = import_data(os.path.dirname(os.path.abspath(__file__)),
+                                'products.csv',
+                                'customers.csv',
+                                'rentals.csv')
+    products = show_available_products()
+    user_data = show_rentals("prd005")
+    print_dict(products)
+    print_dict(user_data)
+    print(added)
+    print(errors)
+
+if __name__ == "__main__":
+    main()
