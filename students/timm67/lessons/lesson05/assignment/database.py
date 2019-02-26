e0e61f515e2599a2db2a8d8885a6aa058198ad48
Tim Meese <tim.meese67@gmail.com>
2019-02-25 23:58:52 -0800

    More assign05 prep

--- students/timm67/lessons/lesson05/assignment/database.py
+++ students/timm67/lessons/lesson05/assignment/database.py
@@ -16,3 +16,42 @@ class MongoDBConnection():
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.connection.close()
 
+def show_available_products():
+    """
+    show_available_products(): Returns a Python dictionary of products
+    listed as available with the following fields:
+        product_id.
+        description.
+        product_type.
+        quantity_available.
+
+    For example:
+
+    {‘prd001’:{‘description’:‘60-inch TV stand’,’product_type’:’livingroom’,
+    ’quantity_available’:‘3’},’prd002’:{‘description’:’L-shaped sofa’,
+    ’product_type’:’livingroom’,’quantity_available’:‘1’}}
+    """
+    pass
+
+
+def show_rentals(product_id):
+    """
+    Returns a Python dictionary with the following user information from users
+    that have rented products matching product_id:
+
+        user_id.
+        name.
+        address.
+        phone_number.
+        email.
+
+    For example:
+
+    {‘user001’:{‘name’:’Elisa Miles’,’address’:‘4490 Union Street’,
+    ’phone_number’:‘206-922-0882’,’email’:’elisa.miles@yahoo.com’},
+    ’user002’:{‘name’:’Maya Data’,’address’:‘4936 Elliot Avenue’,
+    ’phone_number’:‘206-777-1927’,’email’:’mdata@uw.edu’}}
+    """
+    pass
+
+
