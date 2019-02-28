22a10b86242d7da29f7c9c94e350f0084fb8c6f5
Tim Meese <tim.meese67@gmail.com>
2019-02-27 20:09:20 -0800

    Taking out previous assign file deletes

--- students/timm67/lessons/lesson05/assignment/do_csv_import.sh
+++ students/timm67/lessons/lesson05/assignment/do_csv_import.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 export PYTHONPATH=.
-echo 'Removing existing customers.db file...'
-rm -f ./customers.db
-echo 'Removing existing customer.csv file...'
-rm -f ./customer.csv
-echo 'Uncompress/ingest customer.csv file into database...'
+echo 'Ingest CSV files into database...'
 python3 ./main.py
