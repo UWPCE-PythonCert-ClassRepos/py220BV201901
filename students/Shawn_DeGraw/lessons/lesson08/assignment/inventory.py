fe04cebb517387f9cdb4b33a64b4231363219148
motians <motians@comcast.net>
2019-03-06 20:43:51 -0800

    correct doc strings and logging

--- students/Shawn_DeGraw/lessons/lesson08/assignment/inventory.py
+++ students/Shawn_DeGraw/lessons/lesson08/assignment/inventory.py
@@ -1,7 +1,6 @@
 """ Lesson 08 HPNorton furniture rental """
 
 
-from functools import partial
 from pathlib import Path
 import os
 import logging
@@ -20,7 +19,7 @@ SYSTEMLOG.addHandler(FILE_HANDLER_SYSTEM)
 SYSTEMLOG.setLevel("INFO")
 
 def add_furniture(invoice_file, customer_name, item_code, item_description, item_monthly_price):
-    """ Creates the invoice file from the data provided """
+    """ Creates or appends to invoice file using arguments provided """
 
     directory_name = os.path.dirname(os.path.abspath(__file__))
 
@@ -38,7 +37,7 @@ def single_customer(customer_name, invoice_file):
     """ Provides a function """
 
     def consumerentalitems(rental_items):
-        """ Add rental times to file """
+        """ Add rental times to invoice file """
 
         directory_name = os.path.dirname(os.path.abspath(__file__))
         try:
@@ -46,9 +45,9 @@ def single_customer(customer_name, invoice_file):
                 with open(Path(directory_name,rental_items), 'r') as input_file:
 
                     for line in input_file:
-                        fields = line.split(',')
+                        fields = line.strip().split(',')
                         output_file.write(f'{customer_name}, {fields[0]}, {fields[1]}, {fields[2]}\n')
-                        SYSTEMLOG.info(f'Data added to invoice file: {customer_name}, {fields[0]}, {fields[1]}, {fields[2]}')
+                        SYSTEMLOG.info(f'Data added to invoice file {invoice_file}: {customer_name}, {fields[0]}, {fields[1]}, {fields[2]}')
 
         except IOError as fileerror:
             SYSTEMLOG.error(f'File error {directory_name + invoice_file}, exception {type(fileerror).__name__}')
