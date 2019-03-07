83d0ced604b95fee2b1f7f8cd24e6a88e300b417
Tim Meese <tim.meese67@gmail.com>
2019-03-06 20:09:38 -0800

    assign07 added both linear and parallel ingest methods to main

--- students/timm67/lessons/lesson07/assignment/main.py
+++ students/timm67/lessons/lesson07/assignment/main.py
@@ -1,14 +1,23 @@
 from linear import linear
 from parallel import parallel
 
+from models import util_drop_all
+from database import Connection
 
 def main():
     """
     Ensure you application will create an empty database if one doesn’t exist
     when the app is first run. Call it customers.db
     """
+    with Connection():
+        util_drop_all()
 
     linear()
+
+    with Connection():
+        util_drop_all()
+
+    parallel()
 
 if  __name__ == '__main__':
     main()
